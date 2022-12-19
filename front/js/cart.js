b56667bd473a883bdb52ce6fd995a256b0bdf773
DavidIsidore <isidoredavid@protonmail.com>
2022-12-19 14:18:27 +0000

    Changement du logo pour la photo du canapé et du texte alternatif

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,3 +1,10 @@
+//Récupérer liste pdts panier
+
+// Récuoérer liste pdts (du back)
+
+// Boucle affichage chq pdt ajouté au panier
+
+
 // Affichage de la photo du produit
 
 
@@ -8,3 +15,10 @@
 
 // Affichage de la quantité
 
+//Btn suppr / modif
+    //écouteur sur chacun
+
+    //affichage form si panier non vide
+    //écouteur évent sur btn form
+    //varif de chc des chp puis envoi Cde
+    
--- front/js/product.js
+++ front/js/product.js
@@ -2,60 +2,56 @@ url = window.location.href;
 Url = new URL(url);
 var id = Url.searchParams.get("id");
 
-fetch("http://localhost:3000/api/products")
+fetch("http://localhost:3000/api/products/" + id)
   .then(function (response) {
     return response.json();
   })
   .then(function (products) {
     let i = 0;
     let j = 0;
-    const URl = new URL(
-      "../images/logo.png",
-      "http://localhost:5502/front/images/logo.png"
-    );
+
     const item_img = document.querySelector("div.item__img");
-    let texteaffiche = "Photographie d'un canapé";
     let img = document.createElement("img");
-    img.setAttribute("src", "http://localhost:5502/front/images/logo.png");
-    img.setAttribute("alt", "Photographie d'un canapé");
+    img.setAttribute("src", `${products.imageUrl}`); 
+    img.setAttribute("alt", `${products.altTxt}`); 
     item_img.appendChild(img);
 
-    for (products of products) {
-      list_colors = [`${products.colors}`];
-      console.log(`${products.colors}`);
+    //console.log("id de la page" + id);
+    //console.log(i);
+    title = document.getElementById("title");
+    title.innerHTML = `${products.name}`;
+    price = document.getElementById("price");
+    price.innerHTML = `${products.price}`;
+    description = document.getElementById("description");
+    description.innerHTML = `${products.description}`;
+    colors = [`${products.colors}`];
+    //console.log("P" + `${products.colors.length}`);
+    localStorage.setItem("produit", `${products.name}`);
+    var nom = localStorage.getItem("produit");
 
-      id_page = `${products._id}`;
-      id_name = `${products.name}`;
-      if (id != id_page) {
-        i++;
-      } else {
-        console.log("id de la page" + id);
-        console.log(i);
-        title = document.getElementById("title");
-        title.innerHTML = `${products.name}`;
-        price = document.getElementById("price");
-        price.innerHTML = `${products.price}`;
-        description = document.getElementById("description");
-        description.innerHTML = `${products.description}`;
-        colors = [`${products.colors}`];
-        console.log("P" + `${products.colors.length}`);
+    console.log(nom);
 
-        color = document.getElementById("colors");
+    color = document.getElementById("colors");
 
-        for (colors of `${products.colors}`) {
-          while (j != `${products.colors.length}`) {
-            {
-              color.innerHTML +=
-                "<option value=" +
-                `${products.colors[j]}` +
-                ">" +
-                `${products.colors[j]}` +
-                "</option>";
-              console.log(j + ":" + `${products.colors[j]}`);
-              j++;
-            }
-          }
-        }
-      }
+    for (colors of products.colors) {
+      color.innerHTML += "<option value=" + colors + ">" + colors + "</option>";
     }
+
+    const targetButton = document.getElementById("addToCart");
+    targetButton.addEventListener('click',(event)=>{
+        event.preventDefault();
+        //ajout panier
+        const productcolor = document.getElementById("colors").value;
+        const productquantite = document.getElementById("quantity").value;
+        
+        const cart = {
+            productid:products._id,
+            color:productcolor,
+            quantite:productquantite
+        }
+        localStorage.setItem("panier",JSON.stringify(cart));
+        window.confirm("Produit ajouté au panier");
+    });
   });
+
+  
