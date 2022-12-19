93e69b2d4b7e9209432d38807dc6b2813035b08c
DavidIsidore <isidoredavid@protonmail.com>
2022-12-19 10:23:33 +0000

    Suppression des lignes inutiles dans le fichier products.js

--- front/js/product.js
+++ front/js/product.js
@@ -1,53 +1,8 @@
 url = window.location.href;
-//console.log("adresse de la page :"+url);
 Url = new URL(url);
 var id = Url.searchParams.get("id");
-//console.log("id ="+id);
-
-/*fetch("http://localhost:3000/api/products")
-.then(function(response){return response.json();})
-.then(function(products){
-
-    for(products of products){
-        const itemimg = document.createElement("img");
-        itemimg.setAttribute("src","../images/logo.png");
-        itemimg.setAttribute("alt","Photographie d'un canapé}");
-        let test = document.getElementsByClassName("item__img");
-        test.appendChild(itemimg);
-        let test = document.querySelector("div.item__img");
-        //test.innerHTML = "<img src="//localhost:5502/front/images/logo.png" alt="Photographie d'un canapé">";
-
-        let item_title = document.getElementById("title");
-        item_title.innerHTML = `${products.name}`;
-
-        let price = document.getElementById("price");
-        price.innerHTML=`${products.price}`;
-
-        let description = document.getElementById("description");
-        description.innerHTML=`${products.description}`;
-
-        color = `${products.colors.length}`;
-        console.log(color + " couleurs disponibles");
-        colors = document.getElementById("colors");
-        let i=0;
-        while(i<color){
-            i++;
-            colors.innerHTML="<option value="+`${products.colors[i]}`+">"+`${products.colors}`+"</option>";
-            console.log(i);
-            console.log(`${products.colors[i]}`);
-            
-            
-
-        }
 
 
-
-    }
-
-})
-.catch(function(error){
-    alert("Erreur de chargement des produits");
-})*/
 
 fetch("http://localhost:3000/api/products")
 .then(function(response){return response.json();})
@@ -72,13 +27,11 @@ fetch("http://localhost:3000/api/products")
     id_name = `${products.name}`;
     if(id != id_page){
         i++;
-        //console.log(i);
     }else{
         console.log("id de la page"+id);
         console.log(i);
         title = document.getElementById("title");
         title.innerHTML = `${products.name}`;
-        //console.log(`${products.name}`);
         price = document.getElementById("price");
         price.innerHTML =`${products.price}`;
         description = document.getElementById("description");
